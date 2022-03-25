4798aa62c388924af9a5d5e3eae0bb20c63353c2
GiorgioAscione <acergio95@libero.it>
2022-03-25 18:28:45 +0100

    Commit

---
+++ Jsnacks3/script.js
@@ -0,0 +1,23 @@
+alert(" Qui puoi addizionare una serie di numeri ")
+
+let numeroDaInserire1 = parseInt(prompt(" Inserisci un numero da addizionare"));
+let numeroDaInserire2 = parseInt(prompt(" Inserisci un numero da addizionare"));
+let numeroDaInserire3 = parseInt(prompt(" Inserisci un numero da addizionare"));
+let numeroDaInserire4 = parseInt(prompt(" Inserisci un numero da addizionare"));
+let numeroDaInserire5 = parseInt(prompt(" Inserisci un numero da addizionare"));
+let numeroDaInserire6 = parseInt(prompt(" Inserisci un numero da addizionare"));
+let numeroDaInserire7 = parseInt(prompt(" Inserisci un numero da addizionare"));
+let numeroDaInserire8 = parseInt(prompt(" Inserisci un numero da addizionare"));
+let numeroDaInserire9 = parseInt(prompt(" Inserisci un numero da addizionare"));
+let numeroDaInserire10 = parseInt(prompt(" Inserisci un numero da addizionare"));
+
+
+let somma = (numeroDaInserire1 + numeroDaInserire2 + numeroDaInserire3 +
+    numeroDaInserire4 + numeroDaInserire5 + numeroDaInserire6 + numeroDaInserire7 +
+    numeroDaInserire8 + numeroDaInserire9 + numeroDaInserire10);
+    console.log(somma);
+
+    alert(" La somma dei numeri è " + somma);
+
+
+
